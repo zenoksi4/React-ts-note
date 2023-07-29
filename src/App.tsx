fb52a3faa9b0409d17aa6d4efb28638a2840715f
Pasha <zenoken4ik@gmail.com>
2023-07-29 13:04:37 +0300

    added Table components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./App.css";
-
+import Table from "./components/tablelist/Table";
 
 function App() {
   return (
     <div className="container">
-      Hello world!!!
+      <Table/>
     </div>
   );
 }
